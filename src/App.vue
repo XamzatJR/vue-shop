380861b446b0e9f4d591ad37a61de9d3bfd38b22
XamzatJR <bahaev.hamzat95@gmail.com>
2022-04-11 23:37:17 +0300

    add some styles..

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <Home />
 </template>
-
+<script>
+import Home from '@/views/Home.vue';
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+:root {
+  --primary-btn: #535353;
+  --secondary-btn: #d1d1d1;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<template>
+  <header class="header">
+    <nav class="navbar">
+      <div class="navbar-left">
+        <a class="navbar-left__a" href="">Доставка</a>
+        <a class="navbar-left__a" href="">Оплата</a>
+        <a class="navbar-left__a" href="">О нас</a>
+        <a class="navbar-left__a" href="">Бренды</a>
+        <a class="navbar-left__a" href="">Гарантии и возврат</a>
+        <a class="navbar-left__a" href="">Контакты</a>
+        <a class="navbar-left__a" href="">Помощь</a>
+      </div>
+      <div class="navbar-center">
+        <a class="navbar-center__a tel" href="tel:+7495-755-6983">+74957556983</a>
+        <a class="navbar-center__a email" href="mailto:nonamemag@gmail.com">nonamemag@gmail.com</a>
+      </div>
+      <div class="navbar-right">
+        <a class="navbar-right__a" href="">Войти / </a>
+        <a class="navbar-right__a" href="">Зарегистрироваться</a>
+      </div>
+    </nav>
+    <div class="menu">
+      <div class="menu__logo">
+        <img src="@/assets/icons/logo.svg" alt="логотип сайта gift.ru" />
+      </div>
+      <button class="catalog">Каталог</button>
+      <nav>
+        <a href="">Конструктор подарка</a>
+        <a href="">Статьи</a>
+      </nav>
+      <input type="search" name="" id="" />
+      <div class="icons">
+        <img :src="userIcon" alt="" />
+        <img :src="starIcon" alt="" />
+        <img :src="heartIcon" alt="" />
+        <img :src="cartIcon" alt="" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import userIcon from '@/assets/icons/user-icon.svg';
+import starIcon from '@/assets/icons/star-icon.svg';
+import heartIcon from '@/assets/icons/heart-icon.svg';
+import cartIcon from '@/assets/icons/cart-icon.svg';
+export default {
+  setup() {
+    return {
+      userIcon,
+      starIcon,
+      heartIcon,
+      cartIcon,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 45px;
+  background: #f4f1ff;
+  display: flex;
+  padding: 0 109px;
+  align-items: center;
+  &-left {
+    width: 706px;
+    display: flex;
+    justify-content: space-between;
+  }
+  &-center {
+    margin: 0 35px 0 52px;
+    width: 362px;
+    & .tel {
+      margin-right: 19px;
+    }
+    & .tel::before,
+    & .email::before {
+      margin-right: 6px;
+      vertical-align: middle;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+    }
+    & .tel::before {
+      content: url('../assets/icons/telephone.svg');
+    }
+    & .email::before {
+      content: url('../assets/icons/email.svg');
+    }
+  }
+  &-right {
+    width: 228px;
+  }
+}
+</style>
