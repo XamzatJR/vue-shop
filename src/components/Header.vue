05591d1c3011ef70da023c355591b6422ee1dd9c
XamzatJR <bahaev.hamzat95@gmail.com>
2022-04-18 18:37:18 +0300

    Header is fine

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,38 +2,47 @@
   <header class="header">
     <nav class="navbar">
       <div class="navbar-left">
-        <a class="navbar-left__a" href="">Доставка</a>
-        <a class="navbar-left__a" href="">Оплата</a>
-        <a class="navbar-left__a" href="">О нас</a>
-        <a class="navbar-left__a" href="">Бренды</a>
-        <a class="navbar-left__a" href="">Гарантии и возврат</a>
-        <a class="navbar-left__a" href="">Контакты</a>
-        <a class="navbar-left__a" href="">Помощь</a>
+        <a class="a" href="#">Доставка</a>
+        <a class="a" href="#">Оплата</a>
+        <a class="a" href="#">О нас</a>
+        <a class="a" href="#">Бренды</a>
+        <a class="a" href="#">Гарантии и возврат</a>
+        <a class="a" href="#">Контакты</a>
+        <a class="a" href="#">Помощь</a>
       </div>
       <div class="navbar-center">
-        <a class="navbar-center__a tel" href="tel:+7495-755-6983">+74957556983</a>
-        <a class="navbar-center__a email" href="mailto:nonamemag@gmail.com">nonamemag@gmail.com</a>
+        <a class="a tel" href="tel:+7495-755-6983">+74957556983</a>
+        <a class="a email" href="mailto:nonamemag@gmail.com">nonamemag@gmail.com</a>
       </div>
       <div class="navbar-right">
-        <a class="navbar-right__a" href="">Войти / </a>
-        <a class="navbar-right__a" href="">Зарегистрироваться</a>
+        <a class="a" href="#">Войти / </a>
+        <a class="a" href="#">Зарегистрироваться</a>
       </div>
     </nav>
     <div class="menu">
-      <div class="menu__logo">
+      <a href="#" class="menu__logo">
         <img src="@/assets/icons/logo.svg" alt="логотип сайта gift.ru" />
-      </div>
-      <button class="catalog">Каталог</button>
-      <nav>
-        <a href="">Конструктор подарка</a>
-        <a href="">Статьи</a>
+      </a>
+      <button class="catalog">
+        <img :src="catalogIcon" alt="" />
+        Каталог
+      </button>
+      <nav class="nav">
+        <a class="a" href="#">Вопрос-ответ</a>
+        <a class="a" href="#">Статьи</a>
       </nav>
-      <input type="search" name="" id="" />
+      <label class="search">
+        <input class="search__input" placeholder="Поиск" type="text" name="search" />
+        <button class="search__btn"></button>
+      </label>
       <div class="icons">
-        <img :src="userIcon" alt="" />
-        <img :src="starIcon" alt="" />
-        <img :src="heartIcon" alt="" />
-        <img :src="cartIcon" alt="" />
+        <img class="icon" :src="userIcon" alt="" />
+        <img class="icon" :src="starIcon" alt="" />
+        <img class="icon" :src="heartIcon" alt="" />
+        <label class="cart">
+          <img class="icon" :src="cartIcon" alt="" />
+          <span class="cart__counter">1</span>
+        </label>
       </div>
     </div>
   </header>
@@ -44,6 +53,7 @@ import userIcon from '@/assets/icons/user-icon.svg';
 import starIcon from '@/assets/icons/star-icon.svg';
 import heartIcon from '@/assets/icons/heart-icon.svg';
 import cartIcon from '@/assets/icons/cart-icon.svg';
+import catalogIcon from '@/assets/icons/catalog-icon.svg';
 export default {
   setup() {
     return {
@@ -51,25 +61,30 @@ export default {
       starIcon,
       heartIcon,
       cartIcon,
+      catalogIcon,
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.navbar,
+.menu {
+  padding: 0 109px;
+}
 .navbar {
   height: 45px;
   background: #f4f1ff;
   display: flex;
-  padding: 0 109px;
+  justify-content: space-between;
   align-items: center;
+
   &-left {
     width: 706px;
     display: flex;
     justify-content: space-between;
   }
   &-center {
-    margin: 0 35px 0 52px;
     width: 362px;
     & .tel {
       margin-right: 19px;
@@ -92,5 +107,104 @@ export default {
   &-right {
     width: 228px;
   }
+}
+.a {
+  color: var(--header-text-color);
+  font-style: normal;
+  font-weight: 400;
+}
+.menu {
+  height: 95px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &__logo {
+    width: 139px;
+    height: 51px;
+  }
+  & .nav {
+    width: 210px;
+    display: flex;
+    justify-content: space-between;
+    & .a {
+      font-size: 18px;
+    }
+  }
+
+  & .search {
+    position: relative;
+    width: 255px;
+    &__input {
+      width: 220px;
+      border: none;
+      border-bottom: 1px solid #e9e9e9;
+      font-size: 16px;
+      outline: none;
+      &::placeholder {
+        color: #dedede;
+      }
+      &:focus {
+        border-bottom: 1px solid #151313;
+      }
+    }
+    &__btn {
+      position: absolute;
+      bottom: -7px;
+      right: 0;
+      width: 40px;
+      height: 37px;
+      border: none;
+      background: var(--secondary-btn);
+      box-shadow: 0px 4px 10px rgba(255, 169, 39, 0.15);
+      border-radius: 18.5px;
+      &::after {
+        width: 18px;
+        height: 18px;
+        content: url('../assets/icons/loupe-icon.svg');
+      }
+    }
+  }
+  & .icons {
+    width: 220px;
+    display: flex;
+    justify-content: space-between;
+    margin-right: 17px;
+    & .icon {
+      cursor: pointer;
+    }
+    & .cart {
+      position: relative;
+
+      &__counter {
+        position: absolute;
+        top: -15px;
+        right: -15px;
+        color: #fff;
+        font-size: 13px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 17px;
+        height: 17px;
+        background-color: var(--purple-btn);
+        border-radius: 50%;
+      }
+    }
+  }
+}
+.catalog {
+  background-color: var(--rating);
+  border-radius: 77px;
+  width: 169px;
+  height: 49px;
+  border: none;
+  color: #fff;
+  font-weight: 700;
+  font-size: 18px;
+  padding: 0 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
 }
 </style>
