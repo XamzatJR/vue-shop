522395ceb707b3204d4296753fba1e9cf79bde32
XamzatJR <bahaev.hamzat95@gmail.com>
2022-04-20 18:21:43 +0300

    add a card

---
+++ src/components/Card.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="card">
+    <div class="card__top">
+      <div>
+        <svg
+          width="27"
+          height="27"
+          viewBox="0 0 27 27"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_511_142)">
+            <path
+              d="M23.5673 15.8081C23.258 11.7814 21.3832 9.25799 19.7292 7.03123C18.1977 4.96974 16.875 3.18953 16.875 0.563463C16.875 0.352526 16.7569 0.159729 16.5696 0.063067C16.3817 -0.0341752 16.1559 -0.0182494 15.9851 0.105887C13.5011 1.88335 11.4285 4.87914 10.7046 7.73755C10.2019 9.72759 10.1354 11.9648 10.1261 13.4424C7.83216 12.9524 7.31252 9.52108 7.30703 9.48369C7.28119 9.30572 7.17246 9.15083 7.01425 9.06625C6.85441 8.98277 6.66652 8.97671 6.50447 9.05691C6.38418 9.11513 3.55187 10.5543 3.38708 16.3002C3.37553 16.4914 3.375 16.6831 3.375 16.8748C3.375 22.4571 7.91728 26.9992 13.5 26.9992C13.5077 26.9997 13.5159 27.0008 13.5225 26.9992C13.5247 26.9992 13.5269 26.9992 13.5296 26.9992C19.0987 26.9832 23.625 22.4473 23.625 16.8748C23.625 16.5941 23.5673 15.8081 23.5673 15.8081ZM13.5 25.8742C11.6389 25.8742 10.125 24.2616 10.125 22.2792C10.125 22.2117 10.1245 22.1435 10.1294 22.0601C10.1519 21.2241 10.3107 20.6534 10.4848 20.2738C10.8111 20.9747 11.3945 21.619 12.3421 21.619C12.653 21.619 12.9046 21.3674 12.9046 21.0565C12.9046 20.2557 12.9211 19.3318 13.1205 18.4979C13.2979 17.7586 13.722 16.972 14.2592 16.3415C14.4982 17.1599 14.964 17.8223 15.4188 18.4688C16.0698 19.3938 16.7427 20.3501 16.8608 21.9809C16.8679 22.0776 16.8751 22.1749 16.8751 22.2792C16.875 24.2615 15.3611 25.8742 13.5 25.8742Z"
+              fill="#FFA927"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_511_142">
+              <rect width="27" height="27" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      <div class="heart">
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.5 23.6092C12.1441 23.6092 11.8009 23.4802 11.5335 23.246C10.5236 22.3629 9.54989 21.533 8.69082 20.801L8.68643 20.7972C6.16778 18.6508 3.99284 16.7973 2.47955 14.9714C0.787924 12.9301 0 10.9947 0 8.88042C0 6.82621 0.704382 4.93107 1.98326 3.54385C3.27739 2.14024 5.05313 1.36719 6.98393 1.36719C8.42703 1.36719 9.74863 1.82343 10.9119 2.72312C11.499 3.17726 12.0312 3.73306 12.5 4.38137C12.969 3.73306 13.501 3.17726 14.0882 2.72312C15.2515 1.82343 16.5731 1.36719 18.0162 1.36719C19.9468 1.36719 21.7228 2.14024 23.0169 3.54385C24.2958 4.93107 25 6.82621 25 8.88042C25 10.9947 24.2122 12.9301 22.5206 14.9712C21.0073 16.7973 18.8326 18.6506 16.3143 20.7968C15.4537 21.53 14.4785 22.3612 13.4662 23.2464C13.199 23.4802 12.8557 23.6092 12.5 23.6092ZM6.98393 2.83165C5.46702 2.83165 4.07352 3.43704 3.05976 4.53644C2.03094 5.65243 1.46427 7.19509 1.46427 8.88042C1.46427 10.6586 2.12516 12.249 3.60698 14.0369C5.03921 15.7652 7.16952 17.5806 9.6361 19.6827L9.64068 19.6865C10.503 20.4214 11.4805 21.2545 12.4979 22.1441C13.5214 21.2528 14.5004 20.4184 15.3644 19.6823C17.8308 17.5802 19.9609 15.7652 21.3932 14.0369C22.8748 12.249 23.5357 10.6586 23.5357 8.88042C23.5357 7.19509 22.969 5.65243 21.9402 4.53644C20.9266 3.43704 19.5329 2.83165 18.0162 2.83165C16.905 2.83165 15.8848 3.18489 14.9839 3.88145C14.1811 4.50249 13.6219 5.28755 13.294 5.83687C13.1254 6.11935 12.8286 6.28796 12.5 6.28796C12.1713 6.28796 11.8746 6.11935 11.7059 5.83687C11.3783 5.28755 10.819 4.50249 10.016 3.88145C9.1152 3.18489 8.09496 2.83165 6.98393 2.83165Z"
+            fill="#454545"
+          />
+        </svg>
+      </div>
+    </div>
+    <img class="card__img" src="../assets/img/nyam.jpg" alt="poster" />
+    <div class="card__name">Название товара Название товара</div>
+    <div class="card__center">
+      <div class="card__in-stock">В наличии: 15 шт</div>
+      <del class="card__old-price">4600 p.</del>
+    </div>
+    <div class="card__bottom">
+      <ul class="card__rating">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <strong class="card__price">2500 р.</strong>
+    </div>
+    <button class="card__btn-add">Добавить в корзину</button>
+    <button class="card__btn-view">Быстрый просмотр</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 345px;
+  height: 551px;
+  padding: 22px 39px 34px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  &:hover {
+    background-color: #f4f4ff;
+    box-shadow: 6px 11px 36px rgba(0, 0, 0, 0.04);
+    border-radius: 36px;
+    & .card__btn-view {
+      visibility: visible;
+    }
+  }
+  &__top {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    & .heart {
+      cursor: pointer;
+      background-color: var(--bg-color);
+      width: 50px;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+    }
+  }
+  &__center {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  &__bottom {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__rating {
+    list-style: none;
+  }
+
+  &__name {
+    font-weight: 700;
+    font-size: 19px;
+    color: var(--title-text-color);
+  }
+  &__in-stock {
+    font-weight: 400;
+    font-size: 16px;
+    color: var(--tertiary-text-color);
+  }
+  &__old-price {
+    color: #848484;
+    font-weight: 400;
+    font-size: 16px;
+  }
+  &__price {
+    font-weight: 700;
+    font-size: 23px;
+    color: var(--price-color);
+  }
+  &__btn-add {
+    width: 265px;
+    height: 55px;
+    font-weight: 700;
+    font-size: 18px;
+    color: #fff;
+    border: none;
+    background-color: var(--green-btn);
+    border-radius: 193px;
+    cursor: pointer;
+    filter: drop-shadow(3px 5px 14px rgba(22, 193, 161, 0.25));
+  }
+  &__btn-view {
+    border: none;
+    color: var(--red-btn);
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 18px;
+    border-bottom: 1px solid #ff4d4d;
+    visibility: hidden;
+  }
+}
+</style>
