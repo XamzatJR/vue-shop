b1dd9e962e5203f874f24bb7147dcb2733e2b338
XamzatJR <bahaev.hamzat95@gmail.com>
2022-04-27 18:09:26 +0300

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Cart from '../views/Cart.vue';
+import WishList from '../views/WishList.vue';
+import Articles from '../views/Articles.vue';
+import Questions from '../views/Questions.vue';
+import Catalog from '../views/Catalog.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart,
+  },
+  {
+    path: '/wish-list',
+    name: 'WishList',
+    component: WishList,
+  },
+  {
+    path: '/questions-answers',
+    name: 'Q&A',
+    component: Questions,
+  },
+  {
+    path: '/articles',
+    name: 'Articles',
+    component: Articles,
+  },
+  {
+    path: '/catalog',
+    name: 'Catalog',
+    component: Catalog,
   },
   // {
   //   path: "/about",
